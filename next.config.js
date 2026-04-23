e2357861e747feadd551862fe1b05e8faaef1b9f
Febecos - Bombeo Solar <febecos.ar@gmail.com>
2026-04-23 00:09:09 -0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/formulario.html',
+        permanent: false,
+      },
+    ]
+  },
   async headers() {
     return [
       {
